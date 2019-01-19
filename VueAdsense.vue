014053d07972132e974a0dca987c3994f3ceb6d1
rluisr <detteiu.x14th@gmail.com>
2019-01-20 01:25:25 +0900

    support data-full-width-responsive

--- VueAdsense.vue
+++ VueAdsense.vue
@@ -1,36 +1,43 @@
 <template>
-  <ins class="adsbygoogle"
-    :data-ad-client="adClient"
-    :data-ad-slot="adSlot"
-    :data-ad-format="adFormat"
-    :style="adStyle"></ins>
+    <ins class="adsbygoogle"
+         :data-ad-client="adClient"
+         :data-ad-slot="adSlot"
+         :data-ad-format="adFormat"
+         :data-full-width-responsive="adFullWidthResponsive"
+         :style="adStyle">
+    </ins>
 </template>
 
 <script>
-export default {
-  props: {
-    adClient: {
-      type: String,
-      required: true
+  export default {
+    props: {
+      adClient: {
+        type: String,
+        required: true
+      },
+      adSlot: {
+        type: String,
+        required: true
+      },
+      adFormat: {
+        type: String,
+        required: false,
+        default: 'auto'
+      },
+      adFullWidthResponsive: {
+        type: String,
+        required: false,
+        default: 'false'
+      },
+      adStyle: {
+        type: String,
+        required: false,
+        default: 'display: block'
+      }
     },
-    adSlot: {
-      type: String,
-      required: true
-    },
-    adFormat: {
-      type: String,
-      required: false,
-      default: 'auto'
-    },
-    adStyle: {
-      type: String,
-      required: false,
-      default: 'display: block'
+    mounted() {
+      (window.adsbygoogle = window.adsbygoogle || []).push({})
     }
-  },
-  mounted() {
-    (window.adsbygoogle = window.adsbygoogle || []).push({})
   }
-}
 </script>
 
